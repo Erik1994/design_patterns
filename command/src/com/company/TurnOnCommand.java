fc4f938024491321176fd3ab22d58bf899e3e294
Erik Antonyan <erik.antonyan1994@gmail.com>
2021-10-04 09:51:19 +0400

    Implement Command pattern

---
+++ command/src/com/company/TurnOnCommand.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class TurnOnCommand implements Command {
+    private Light light;
+
+    public TurnOnCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        this.light.turnOn();
+    }
+}
